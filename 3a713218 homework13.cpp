ae64e859265d2b0cc2b4a2cfe4833ab79d312396
Wesleylin0312 <48340640+Wesleylin0312@users.noreply.github.com>
2019-03-22 11:25:31 +0800

    Add files via upload

---
+++ 3a713218 homework13.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+#include<stdlib.h>
+int main()
+{
+    int a,b;
+	printf("請輸入男生年紀: \n");
+	scanf("%d",&a);
+	printf("請輸入女生年紀: \n");
+	scanf("%d",&b);
+	
+	
+   if(a>=18&b>=16)
+   printf("恭喜你離開單身狗動物之家\n");
+   else
+   printf("十年一千萬，土城看守所歡迎您\n");
+   system("PAUSE");
+   return 0;
+    
+   
+   
+   
+	
+	
+
+	
+}
